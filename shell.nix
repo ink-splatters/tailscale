cb036f8f596d921c614c23a65b27f7c431af0e87
Peter A. <ink.splatters@pm.me>
2026-02-17 04:44:02 +0100

    formatted shell.nix

--- shell.nix
+++ shell.nix
@@ -8,11 +8,13 @@
 # automatically get your environment set up when you change folders into the
 # project.
 (import (
-  let
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  in fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-    sha256 = lock.nodes.flake-compat.locked.narHash; }
-) {
-  src =  ./.;
-}).shellNix
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+      fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+        sha256 = lock.nodes.flake-compat.locked.narHash;
+      }
+  ) {
+    src = ./.;
+  }).shellNix
