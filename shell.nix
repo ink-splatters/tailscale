8644d6e2af4349f6e05149339a6b0c88a72a964b
Peter A. <ink.splatters@pm.me>
2026-03-21 14:43:59 +0100

    nix: switch to flake-parts and tailscale-go toolchain flake
    - move dev-only outputs to `nix/dev` and add `nix/flake-module.nix`
    - use `ink-splatters/tailscale-go`
    - use upstream flakehashes.json for goModSum and SRI
    - remove (now irrelevant) toolchain metadata files and helper/update scripts
    - simplify toolchain/version embedding
    - `.envrc` (nix-direnv)
    
    Signed-off-by: Peter A. <ink.splatters@pm.me>

--- shell.nix
+++ shell.nix
@@ -8,12 +8,13 @@
 # automatically get your environment set up when you change folders into the
 # project.
 (import (
-  let
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  in fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-    sha256 = lock.nodes.flake-compat.locked.narHash; }
-) {
-  src =  ./.;
-}).shellNix
-# nix-direnv cache busting line: sha256-Xwm+ZLNqd2k7c2GFQJ2Pf/xuFLMcXhYl5I/YVgS9V4U=
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+      fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+        sha256 = lock.nodes.flake-compat.locked.narHash;
+      }
+  ) {
+    src = ./.;
+  }).shellNix
