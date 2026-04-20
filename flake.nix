5b1464fc062609999036ef6108c9aa8f5883f34d
Peter A. <ink.splatters@pm.me>
2026-04-20 08:22:06 +0200

    • Updated input 'tailscale-go':     'github:ink-splatters/tailscale-go/e5277435d4e6454c1da47e73b88293b740f0f29d?narHash=sha256-Wr1GNqMIvWS5sWsRitFGvqExZ298MuS9QulpBVCLZ8Y%3D' (2026-03-21)   → 'github:ink-splatters/tailscale-go/15729d5cf00d608aacb59c88f712f3bda4f3488e?narHash=sha256-U5uT41EYJJMt0ftb%2BQjQQgk7cYVPaRKWOlrLolFARcM%3D' (2026-04-17)     (go1.26.2+tailscale.20260407T125051Z)

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     };
 
     tailscale-go = {
-      url = "github:ink-splatters/tailscale-go/go1.26.1+tailscale.20260321T130617Z";
+      url = "github:ink-splatters/tailscale-go/go1.26.2+tailscale.20260407T125051Z";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         systems.follows = "systems";
